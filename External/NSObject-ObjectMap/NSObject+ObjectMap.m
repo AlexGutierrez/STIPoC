5a867ec62e0f020943375fd32baea6db85e5b2e4
Alex Gutiérrez <alex.gutierrez@avantica.net>
2013-12-26 11:38:50 -0500

    Analyzer fixes

--- External/NSObject-ObjectMap/NSObject+ObjectMap.m
+++ External/NSObject-ObjectMap/NSObject+ObjectMap.m
@@ -829,7 +829,8 @@ static const char * getPropertyType(objc_property_t property) {
 
 +(NSData *)base64DataFromString:(NSString *)string {
     unsigned long ixtext, lentext;
-    unsigned char ch, inbuf[4], outbuf[3];
+    unsigned char ch, outbuf[3];
+    unsigned char inbuf[4] = { };
     short i, ixinbuf;
     Boolean flignore, flendtext = false;
     const unsigned char *tempcstring;
@@ -916,9 +917,10 @@ static const char * getPropertyType(objc_property_t property) {
     return theData;
 }
 
+/*
 + (NSString *)encodeBase64WithData:(NSData *)objData {
     const unsigned char * objRawData = [objData bytes];
-    char * objPointer;
+    char * objPointer = NULL;
     char * strResult;
     
     // Get the Raw Data length and ensure we actually have data
@@ -962,8 +964,38 @@ static const char * getPropertyType(objc_property_t property) {
     // Return the results as an NSString object
     return [NSString stringWithCString:strResult encoding:NSUTF8StringEncoding];
 }
+*/
 
-
++ (NSString *)encodeBase64WithData:(NSData *)objData {
+    const uint8_t* input = (const uint8_t*)[objData bytes];
+    NSInteger length = [objData length];
+    
+    static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    
+    NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
+    uint8_t* output = (uint8_t*)data.mutableBytes;
+    
+    NSInteger i;
+    for (i=0; i < length; i += 3) {
+        NSInteger value = 0;
+        NSInteger j;
+        for (j = i; j < (i + 3); j++) {
+            value <<= 8;
+            
+            if (j < length) {
+                value |= (0xFF & input[j]);
+            }
+        }
+        
+        NSInteger theIndex = (i / 3) * 4;
+        output[theIndex + 0] =                    table[(value >> 18) & 0x3F];
+        output[theIndex + 1] =                    table[(value >> 12) & 0x3F];
+        output[theIndex + 2] = (i + 1) < length ? table[(value >> 6)  & 0x3F] : '=';
+        output[theIndex + 3] = (i + 2) < length ? table[(value >> 0)  & 0x3F] : '=';
+    }
+    
+    return [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+}
 
 @end
 
