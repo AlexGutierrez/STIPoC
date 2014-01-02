5bae584ee03d93a2c298be8b43f703f13639c8ad
Alex Gutiérrez <alex.gutierrez@avantica.net>
2014-01-02 15:15:57 -0500

    AT-5: Attributes ordering popover

---
+++ STIPoC/Presentation/Controllers/CMDB/Attributes/OrderPriorityPopoverViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrderPriorityPopoverViewController.h
+//  STIPoC
+//
+//  Created by Alex Gutierrez on 1/2/14.
+//  Copyright (c) 2014 Administrator. All rights reserved.
+//
+
+#import "GenericViewController.h"
+
+@interface OrderPriorityPopoverViewController : GenericViewController
+
+@end
---
+++ STIPoC/Presentation/Controllers/CMDB/Attributes/OrderPriorityPopoverViewController.m
@@ -0,0 +1,38 @@
+//
+//  OrderPriorityPopoverViewController.m
+//  STIPoC
+//
+//  Created by Alex Gutierrez on 1/2/14.
+//  Copyright (c) 2014 Administrator. All rights reserved.
+//
+
+#import "OrderPriorityPopoverViewController.h"
+
+@interface OrderPriorityPopoverViewController ()
+
+@end
+
+@implementation OrderPriorityPopoverViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
